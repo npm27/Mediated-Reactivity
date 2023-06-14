926d0a92cb526d413fb4d972d57a72c4367be88a
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2023-06-14 15:32:11 -0500

    Add in signal detection

--- 4 Analyses/Signal Detection/Signal detection analyses.R
+++ 4 Analyses/Signal Detection/Signal detection analyses.R
@@ -6,6 +6,9 @@ ex4 = read.csv("Sig Data/ex4_sig.csv")
 ##turn off scientific notation
 options(scipen = 999)
 
+#load libraries
+library(ez)
+
 ####Ex 2####
 ###Make JOL and Read Subsets
 JOL2 = subset(ex2,
@@ -14,8 +17,12 @@ Read2 = subset(ex2,
                ex2$e == "Read")
 
 ###get means
-mean(JOL2$dprime); mean(Read2$dprime) #1.9 vs. 1.03
+mean(JOL2$dprime); mean(Read2$dprime) #1.86 vs. 1.03
 mean(JOL2$c.1); mean(Read2$c.1) #.06 vs. .07
+
+#get sds
+sd(JOL2$dprime); sd(Read2$dprime) 
+sd(JOL2$c.1); sd(Read2$c.1) 
 
 ##d'
 temp = t.test(JOL2$dprime, Read2$dprime, paired = F, p.adjust.methods = "bonferroni", var.equal = T)
@@ -31,6 +38,18 @@ round(temp$p.value, 3)
 temp$statistic #sig!
 (temp$conf.int[2] - temp$conf.int[1]) / 3.92 #No difference in C
 
+#pbic
+pbic = rbind(JOL2[ , c(1, 2, 11)], Read2[, c(1, 2, 11)])
+
+model1 = ezANOVA(pbic,
+                 wid = i,
+                 between = e,
+                 dv = c.1,
+                 detailed = T,
+                 type = 3)
+
+model1
+
 #####Ex 3####
 ###Make JOL and Read Subsets
 JOL3 = subset(ex3,
@@ -41,6 +60,10 @@ Read3 = subset(ex3,
 ###get means
 mean(JOL3$dprime); mean(Read3$dprime) #1.65 vs. 1.12
 mean(JOL3$c.1); mean(Read3$c.1) #.2 vs. .19
+
+#get sds
+sd(JOL3$dprime); sd(Read3$dprime) 
+sd(JOL3$c.1); sd(Read3$c.1) 
 
 ##d'
 temp = t.test(JOL3$dprime, Read3$dprime, paired = F, p.adjust.methods = "bonferroni", var.equal = T)
@@ -56,6 +79,18 @@ round(temp$p.value, 3)
 temp$statistic #sig!
 (temp$conf.int[2] - temp$conf.int[1]) / 3.92 #No difference in C
 
+#pbic
+pbic2 = rbind(JOL3[ , c(1, 2, 11)], Read3[, c(1, 2, 11)])
+
+model2 = ezANOVA(pbic2,
+                 wid = i,
+                 between = e,
+                 dv = c.1,
+                 detailed = T,
+                 type = 3)
+
+model2
+
 ####Ex 4####
 ###Make JOL and Read Subsets
 JOL4 = subset(ex4,
@@ -66,6 +101,10 @@ Read4 = subset(ex4,
 ###get means
 mean(JOL4$dprime); mean(Read4$dprime) #1.49 vs. 1.09
 mean(JOL4$c.1); mean(Read4$c.1) #.10 vs. .17
+
+#get sds
+sd(JOL3$dprime); sd(Read3$dprime) 
+sd(JOL3$c.1); sd(Read3$c.1) 
 
 ##d'
 temp = t.test(JOL4$dprime, Read4$dprime, paired = F, p.adjust.methods = "bonferroni", var.equal = T)
@@ -80,3 +119,15 @@ temp
 round(temp$p.value, 3)
 temp$statistic #sig!
 (temp$conf.int[2] - temp$conf.int[1]) / 3.92 #No difference in C
+
+#pbic
+pbic3 = rbind(JOL4[ , c(1, 2, 11)], Read4[, c(1, 2, 11)])
+
+model3 = ezANOVA(pbic3,
+                 wid = i,
+                 between = e,
+                 dv = c.1,
+                 detailed = T,
+                 type = 3)
+
+model3
